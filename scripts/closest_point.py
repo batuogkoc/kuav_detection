aefbf42697613cd5b825d26eb799d593433af07e
batuogkoc <bgunduz21@ku.edu.tr>
2023-02-06 17:13:08 +0300

    vectorised the closest_point algorithm, got a 100 fold speedup

--- scripts/closest_point.py
+++ scripts/closest_point.py
@@ -1,59 +1,14 @@
-from numpy import array, cross
+from numpy import ndarray, array, cross
+import numpy as np
 from numpy.linalg import solve, norm
+import time
 
 
 
 
-# # verilerin geldiği liste
-# liste= [[]]
-
-# # verileri temizlemek için kullanılacak uzaklık threshold
-# d= 1000
-
-# ayıklama olmadan depolanan centerlar
-centers=[]
-
-
-def ray_center(XA0,XA1,XB0,XB1):
-
-    #2 noktadan geçen doğru için gereken noktalar
-    
-    
-    
-    # bu noktalardan geçen vektör
-    
-    UA = (XA1 - XA0) / norm(XA1 - XA0)
-    UB = (XB1 - XB0) / norm(XB1 - XB0)
-    
-    #matematik
-    
-    UC = cross(UB, UA)
-    UC /= norm(UC)
-    
-    
-    RHS = XB0 - XA0
-    LHS = array([UA, -UB, UC]).T
-    t=solve(LHS, RHS)
-    
-    # ilk doğrudan geçen nokta
-    
-    D1 = XA0 + t[0]*UA
-    
-    # ikinci doğrudan geçen nokta
-    
-    D2 = XB0 + t[1]*UB
-    
-    # merkez noktası
-    
-    center= (D1+D2)/2
-    
-    # print(center)
-    
-    return center,D1,D2
-
-def detect_center(ray_list, d=1000):
+def __detect_center_old(ray_list, d=1000):
     ray_count = len(ray_list)
-    print(ray_count)
+    # print(ray_count)
     """
         ray_list: [[two points on ray0], [two points on ray1], ...]
     """
@@ -94,19 +49,167 @@ def detect_center(ray_list, d=1000):
     one_true_center=one_true_center/len(real_centers)
 
     return one_true_center
+
+def __ray_center(ray_0_point_0,ray_0_point_1,ray_1_point_0,ray_1_point_1):
+
+    #2 noktadan geçen doğru için gereken noktalar
+       
+    
+    # bu noktalardan geçen vektör
+    ray_0_direction = (ray_0_point_1 - ray_0_point_0) / norm(ray_0_point_1 - ray_0_point_0)
+    ray_1_direction = (ray_1_point_1 - ray_1_point_0) / norm(ray_1_point_1 - ray_1_point_0)
+    # print(norm(ray_1_point_1 - ray_1_point_0, axis=1))
+    #matematik
+    
+    UC = cross(ray_1_direction, ray_0_direction)
+    UC /= norm(UC)
+    
+    
+    RHS = ray_1_point_0 - ray_0_point_0
+    LHS = array([ray_0_direction, -ray_1_direction, UC]).T 
+    
+    t=solve(LHS, RHS)
+
+    # ilk doğrudan geçen nokta
+    
+    D1 = ray_0_point_0 + t[0]*ray_0_direction
+    
+    # ikinci doğrudan geçen nokta
+    
+    D2 = ray_1_point_0 + t[1]*ray_1_direction
+    
+    # merkez noktası
+    
+    center= (D1+D2)/2
+    
+    # print(center)
+    
+    return center,D1,D2
+
+
+
+def multi_ray_center(ray_0_point_0,ray_0_point_1,other_rays_point_0,other_rays_point_1):
+    ray_count = other_rays_point_0.shape[1]
+    #2 noktadan geçen doğru için gereken noktalar
+       
+    # bu noktalardan geçen vektör
+    ray_0_direction = (ray_0_point_1 - ray_0_point_0) / norm(ray_0_point_1 - ray_0_point_0)
+    other_rays_direction = (other_rays_point_1 - other_rays_point_0) / norm(other_rays_point_1 - other_rays_point_0, axis=0)
+
+    # #matematik
+    
+    UC = cross(other_rays_direction, ray_0_direction, axis=0)
+    UC /= norm(UC, axis=0)
+    
+    
+    RHS = other_rays_point_0 - ray_0_point_0
+    a = np.broadcast_to(ray_0_direction, other_rays_direction.shape)
+    b=-other_rays_direction
+    c=UC
+    LHS = np.stack([a, b, c], axis=0).T 
+    RHS = RHS.T.reshape(other_rays_direction.shape[1],3,1)
+    t=solve(LHS, RHS)
+    
+    # ilk doğrudan geçen nokta
+    
+    D1 = ray_0_point_0 + np.broadcast_to(ray_0_direction, (3,ray_count)) * t[:,0,:].T
+
+    # ikinci doğrudan geçen nokta
+    
+    D2 = other_rays_point_0 + other_rays_direction * t[:,1,:].T
+    
+    # merkez noktası
+    
+    center= (D1+D2)/2
+    
+    
+    return center,D1,D2   
+
+def detect_center(ray_list, d=1000):
+    ray_count = len(ray_list)
+    # print(ray_count)
+    """
+        ray_list: [[two points on ray0], [two points on ray1], ...]
+    """
+    centers = None
+    ray_points_0 = np.hstack([ray[0].reshape(3,1) for ray in ray_list])
+    ray_points_1 = np.hstack([ray[1].reshape(3,1) for ray in ray_list])
+    # doğrulardaki en yakın noktaların birbirinden uzak olması durumunda uygulanan ayıklama
+    for i in range(0,len(ray_list)):
+        ray_0_point_0 = ray_points_0[:,i:i+1]
+        ray_0_point_1 = ray_points_1[:,i:i+1]
+        other_rays_point_0 = ray_points_0[:,i+1:]
+        other_rays_point_1 = ray_points_1[:,i+1:]
+        res = multi_ray_center(ray_0_point_0, ray_0_point_1, other_rays_point_0, other_rays_point_1)
+        centers_interim = res[0]
+        D1 = res[1]
+        D2 = res[2]
+        # print(centers_interim)
+        centers_interim = centers_interim.T[np.where(norm(D1-D2,axis=0)<d)].T
+        if centers is None:
+            centers = centers_interim
+        else:
+            centers = np.hstack((centers, centers_interim))
+
+    if centers.shape[1] == 0:
+        return None
+
+    # ikinci ayıklamadan sonra verilerin depolanacağı liste
+    real_centers=None
+
+    #doğrulardaki en yakın noktaların birbirine yakın ama diğer her noktaya uzak olması durumunda
+    # uygulanan ayıklama, mantığı least square mantığına benziyor
+    center_count = centers.shape[1]
+    a = centers.T.reshape(1,center_count,3)
+    b = centers.T.reshape(center_count,1,3)
+    score = np.sum(norm(a - b, axis=2), axis=0) / center_count
+    real_centers = centers.T[np.where(score<d)].T
             
+            
+    # print(real_centers)
+    if real_centers.shape[1] ==0:
+        return None
+
+    return np.average(real_centers, axis=1)
+
         
 if __name__=="__main__":
-    XA0 = array([1, 0, 0])
-    XA1 = array([1, 2, 3])
-    XB0 = array([1, 0, 0])
-    XB1 = array([1, 2, 4])
+    # XA0 = array([[1, 0, 0]]).T
+    # XA1 = array([[1, 2, 3]]).T
+    # XB0 = array([[1, 0, 0]]).T
+    # XB1 = array([[1, 2, 4]]).T
+    # XC0 = array([[1, 0, 0]]).T
+    # XC1 = array([[1, 2, 5]]).T
+    
+    # center = ray_center(XA0.reshape(3),XA1.reshape(3),XB0.reshape(3),XB1.reshape(3))
+    # print(center)
 
-    center = ray_center(XA0,XA1,XB0,XB1)
-    print(center[0])
+    # center=multi_ray_center(XA0,XA1,np.hstack((XB0,XC0)),np.hstack((XB1,XC1)))
+    # print(center)
 
-    center=detect_center([(XA0,XA1),(XB0,XB1)])
-    print(center)
+    # N=20
+    # ray_0_point_0 = np.random.rand(3,1)
+    # ray_0_point_1 = np.random.rand(3,1)
+    # other_rays_point_0 = np.random.rand(3,N)
+    # other_rays_point_1 = np.random.rand(3,N)
+
+    # centers = multi_ray_center(ray_0_point_0, ray_0_point_1, other_rays_point_0, other_rays_point_1)[0]
+    # # print(centers)
+    # for i in range(N):
+    #     center = ray_center(ray_0_point_0.reshape(3), ray_0_point_1.reshape(3), other_rays_point_0[:,i].reshape(3), other_rays_point_1[:,i].reshape(3))[0]
+    #     print(np.allclose(center, centers[:,i]))
+
+    N=50
+    d=0.5
+    rays = [[np.random.rand(3) for i in range(2)] for j in range(N)]
+
+    start = time.perf_counter()
+    print(detect_center(rays, d))
+    print(time.perf_counter()-start)
+
+    start = time.perf_counter()
+    print(detect_center_new(rays, d))
+    print(time.perf_counter()-start)
         
         
         
